398b7f03424a7a1e3a5df7a2a629a9d1d9a57459
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2018-08-28 18:47:35 +0200

    fix(sync-utils): Change utils to match main repo and learn

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 export function dasherize(name) {
   return ('' + name)
     .toLowerCase()
+    .trim()
     .replace(/\s/g, '-')
     .replace(/[^a-z0-9\-\.]/gi, '')
-    .replace(/\:/g, '');
+    .replace(/\./g, '-');
 }
